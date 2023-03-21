7d1d36593122e7e086eefa5986d46ed632bacc22
netartporter <126806454+netartporter@users.noreply.github.com>
2023-03-21 11:07:09 +0100

    Add files via upload

---
+++ jean/js/etape1.js
@@ -0,0 +1,18 @@
+// ** VARIABLES
+
+// ** DOM READY
+$(function () {
+	console.log('dom ready');
+	
+	$("#coton").click(function(){
+	
+			$("#coton").hide();
+			$("#bouteille").show();
+	
+
+	});
+	
+});
+// ** FUNCTIONS
+
+
